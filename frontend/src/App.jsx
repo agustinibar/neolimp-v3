e97dcca74cb66d8d361f4e1db828f853914f2928
Ibar Agustin Ignacio <agustin.ibar@arba.gov.ar>
2026-08-10 17:16:37 -0300

    faltaba import en App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import { Route, Routes } from 'react-router-dom';
 
 import Header from '@/components/Header';
 import Hero from '@/components/Hero';
-import Services from '@/components/Services';
 import Clients from '@/components/Clients';
 import Process from '@/components/Process';
 import Contact from '@/components/Contact';
